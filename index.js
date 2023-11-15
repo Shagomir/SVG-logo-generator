bf85e8633142b9bef9e00ec196ebfee010715ee3
Shagomir <m.j.knudson@gmail.com>
2023-11-14 19:48:40 -0600

    commenting on code

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
+//including dependencies
 const inquirer = require("inquirer");
-
 const questions = require("./lib/questions");
-
-
 const { Circle, Square, Triangle } = require("./lib/shapes");
 
-
-
+//this function calls inquirer, then uses the answer hash to create the object and generate the SVG file. 
 function init() {
   inquirer.prompt(questions).then((answers) => {
     let userAnswers = answers;
     // console.log(userAnswers);
     switch (userAnswers.shape) {
       case "Circle":
-        userShape = new Circle (userAnswers.filename, userAnswers.shapeColor, userAnswers.textColor, userAnswers.logoText)
+        userShape = new Circle(
+          userAnswers.filename,
+          userAnswers.shapeColor,
+          userAnswers.textColor,
+          userAnswers.logoText
+        );
         break;
       case "Square":
-        userShape = new Square (userAnswers.filename, userAnswers.shapeColor, userAnswers.textColor, userAnswers.logoText)
+        userShape = new Square(
+          userAnswers.filename,
+          userAnswers.shapeColor,
+          userAnswers.textColor,
+          userAnswers.logoText
+        );
         break;
       case "Triangle":
-        userShape = new Triangle (userAnswers.filename, userAnswers.shapeColor, userAnswers.textColor, userAnswers.logoText)
+        userShape = new Triangle(
+          userAnswers.filename,
+          userAnswers.shapeColor,
+          userAnswers.textColor,
+          userAnswers.logoText
+        );
         break;
-      default: userShape = new Circle (userAnswers.filename, userAnswers.shapeColor, userAnswers.textColor, userAnswers.logoText)
+      //this should never be used, but if there is an error it defaults to using a circle.
+      default:
+        userShape = new Circle(
+          userAnswers.filename,
+          userAnswers.shapeColor,
+          userAnswers.textColor,
+          userAnswers.logoText
+        );
         break;
     }
-    userShape.writeToFile()
+    //call the function inside of the created object to write the SVG file. 
+    userShape.writeToFile();
   });
 }
 
